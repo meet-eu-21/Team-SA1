63b1f8be229073efe61a12b241461a32567e1c38
LiamLeGoffic <liamlegoffic@yahoo.fr>
2021-12-15 18:13:40 +0100

    remove print

--- consensus.py
+++ consensus.py
@@ -8,7 +8,6 @@ def consensus(list1, list2, resolution):
         for i in range(-gap, gap+1):
             for j in range(-gap, gap+1):
                 if (tad[0]+i, tad[1]+j) in list2:
-                    #print(tad, (tad[0]+i, tad[1]+j))
-                    #print((min(tad[0], tad[0]+i), max(tad[1], tad[1]+j)))
                     true_TADs.append((min(tad[0], tad[0]+i), max(tad[1], tad[1]+j)))
     return true_TADs
+

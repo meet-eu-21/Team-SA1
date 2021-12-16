bceca7d51e3f937ee06a742a02034d4ad00d7f08
LiamLeGoffic <liamlegoffic@yahoo.fr>
2021-12-16 10:28:02 +0100

    Update utils.py
    arrondi au lieu de partie entiere pour les coordonnées des tads+rendre utilisable la fonction pour d'autres chromosomes que le premier

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,8 @@ def read_arrowhead_result(path, chromosome, resolution):
     # TODO: Refactor
     df = pd.read_csv(path, sep='\t')
     df = df[df['chr1']==chromosome]
+    index_chr = df.index.tolist()
     list_tads = []
-    for i in range(len(df)):
-        list_tads.append((int(df['x1'][i]/resolution), int(df['x2'][i]/resolution)))
+    for i in index_chr:
+        list_tads.append((int(round(df['x1'][i]/resolution), 0), int(round(df['x2'][i]/resolution), 0)))
     return list_tads

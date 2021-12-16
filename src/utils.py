80d37b72d45f5dfea15d92ff7f46c59f4add600c
abakarovaMarina <92219026+abakarovaMarina@users.noreply.github.com>
2021-12-16 10:42:48 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,5 +7,5 @@ def read_arrowhead_result(path, chromosome, resolution):
     df = df[df['chr1']==chromosome]
     list_tads = []
     for i in df.index.tolist():
-        list_tads.append((int(round(df['x1'][i]/resolution), 0), int(round(df['x2'][i]/resolution), 0)))
+        list_tads.append((int(round(df['x1'][i]/resolution, 0)), int(round(df['x2'][i]/resolution, 0))))
     return list_tads

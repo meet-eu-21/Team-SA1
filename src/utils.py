73d2c2e9406749650fe2aabc27282ca6cc826d38
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-08 00:52:31 +0100

    Correct read_arrowhead_result()

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
+import os
 
 # load the available ArrowHead results
-def read_arrowhead_result(path, chromosome, resolution):
+def read_arrowhead_result(path, chromosome):
     # TODO: Refactor
-    df = pd.read_csv(path, sep='\t')
+    df = pd.read_csv(path, sep='\t', header=0)
     df = df[df['chr1']==chromosome]
     list_tads = []
     for i in df.index.tolist():
-        list_tads.append((int(round(df['x1'][i]/resolution, 0)), int(round(df['x2'][i]/resolution, 0))))
+        list_tads.append((int(df['x1'][i]), int(df['x2'][i])))
     return list_tads
 
 def SCN(D, max_iter = 10):
@@ -21,4 +22,4 @@ def SCN(D, max_iter = 10):
 	for i in range(max_iter):
 		D = np.divide(D, np.maximum(1, D.sum(axis = 0)))       
 		D = np.divide(D, np.maximum(1, D.sum(axis = 1)))    
-	return (D + D.T)/2 # To make matrix symetric again   
+	return (D + D.T)/2 # To make matrix symetric again

3ed8b127eb9f589cde73f947789c7942630e4681
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-11 22:35:52 +0100

    Update tuning.sbatch

--- tuning.sh
+++ tuning.sh
@@ -9,9 +9,14 @@ import src.metrics
 import src.tad_algo
 from src.tuning import tune_topdom, tune_tadtree
 
-print('Tuning TopDom...')
-print('\tParameter window')
-tune_topdom(development_set)
+print('Init dataset')
+dataset = HiCDataset(data_folder='data')
+dataset.build_data_dict()
+development_set, test_set = dataset.split(dev_ratio = 0.7, test_ratio=0.3)
+
+# print('Tuning TopDom...')
+# print('\tParameter window')
+# tune_topdom(development_set)
 print('Tuning TADTree...')
 print('\tParameter N')
 tune_tadtree(development_set, param_ranges={'N': (100,600)})

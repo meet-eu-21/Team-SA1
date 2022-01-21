00b1f7f5cddac473107fab6d95f04c3ef851f90c
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-21 20:20:04 +0100

    Implement OnTAD tuning

--- src/tuning.py
+++ src/tuning.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
-from src.tad_algo import TopDom, TADtree
+from src.tad_algo import TopDom, TADtree, OnTAD, TADbit
 from src.metrics import compare_to_groundtruth
 from src.data import load_hic_groundtruth
 
@@ -152,4 +152,104 @@ def tune_tadtree(development_set, param_ranges={'p': (2,5), 'N': (100,600)}):
         ax.set_title('TADtree on 100kb intrachromosomal HiC data')
         ax.legend()
         plt.savefig('figures/tune_tadtree_p{}-{}.png'.format(param_ranges['p'][0], param_ranges['p'][1]))
-        
+
+
+def tune_ontad(development_set, param_ranges={'penalty': (0.05,0.35), 'log2': (True,False)}):
+    logging.info('Tuning OnTAD on intrachromosomal HiC data')
+    set_25kb = []
+    set_100kb = []
+    for f in development_set:
+        if '25kb' in f:
+            set_25kb.append(f)
+        elif '100kb' in f:
+            set_100kb.append(f)
+        else:
+            raise ValueError('File name {} was unexpected'.format(f))
+    
+    if 'penalty' in param_ranges:
+        penalty_range = range(param_ranges['penalty'][0], param_ranges['penalty'][1], 0.05) # Step of 0.05
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(20,10))
+        gt_rates_25kb, pred_rates_25kb = np.zeros((len(penalty_range), len(set_25kb))), np.zeros((len(penalty_range), len(set_25kb)))
+        gt_rates_100kb, pred_rates_100kb = np.zeros((len(penalty_range), len(set_100kb))), np.zeros((len(penalty_range), len(set_100kb)))
+        for i, penalty in enumerate(tqdm(penalty_range)):
+            print('OnTAD tuning - penalty: {}'.format(penalty))
+            with contextlib.redirect_stdout(io.StringIO()) as f:
+                for j, f_25kb in enumerate(set_25kb):
+                    hic_mat, arrowhead_tads = load_hic_groundtruth(f_25kb, 25000)
+                    ontad = OnTAD()
+                    ontad_tads = ontad.getTADs(hic_mat, penalty=penalty)
+                    _, _, gt_rate_ontad, pred_rate_ontad = compare_to_groundtruth(ground_truth=arrowhead_tads, predicted_tads=ontad_tads, gap=200000)
+                    gt_rates_25kb[i,j] = gt_rate_ontad
+                    pred_rates_25kb[i,j] = pred_rate_ontad
+                for j, f_100kb in enumerate(set_100kb):
+                    hic_mat, arrowhead_tads = load_hic_groundtruth(f_100kb, 100000)
+                    ontad = OnTAD()
+                    ontad_tads = ontad.getTADs(hic_mat, penalty=penalty)
+                    _, _, gt_rate_ontad, pred_rate_ontad = compare_to_groundtruth(ground_truth=arrowhead_tads, predicted_tads=ontad_tads, gap=200000)
+                    gt_rates_100kb[i,j] = gt_rate_ontad
+                    pred_rates_100kb[i,j] = pred_rate_ontad
+        print('\OnTAD tuning - plotting')
+        pred_rates_25kb = pred_rates_25kb.mean(axis=1)
+        pred_rates_100kb = pred_rates_100kb.mean(axis=1)
+        gt_rates_25kb = gt_rates_25kb.mean(axis=1)
+        gt_rates_100kb = gt_rates_100kb.mean(axis=1)
+        ax1.plot(penalty_range, gt_rates_25kb, label='Rate of Ground Truth TADs correctly predicted by OnTAD')
+        ax1.plot(penalty_range, pred_rates_25kb, label='Rate of Predicted TADs by OnTAD present in Ground Truth')
+        ax1.set_xlabel('Penalty')
+        ax1.set_ylabel('Rate')
+        ax1.set_title('OnTAD on 25kb intrachromosomal HiC data')
+        ax1.legend()
+        ax2.plot(penalty_range, gt_rates_100kb, label='Rate of Ground Truth TADs correctly predicted by OnTAD')
+        ax2.plot(penalty_range, pred_rates_100kb, label='Rate of Predicted TADs by OnTAD present in Ground Truth')
+        ax2.set_xlabel('Penalty')
+        ax2.set_ylabel('Rate')
+        ax2.set_title('OnTAD on 100kb intrachromosomal HiC data')
+        ax2.legend()
+        plt.savefig('figures/tune_ontad_penalty{}-{}.png'.format(param_ranges['penalty'][0], param_ranges['penalty'][1]))
+
+    if 'log2' in param_ranges:
+        log2_range = [True, False]
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(20,10))
+        results_df = pd.DataFrame({'resolution':[25000, 25000, 100000, 100000], 'log2':[True, False, True, False], 'pred_rates':[0,0,0,0], 'gt_rates':[0,0,0,0]})
+        for i, log2 in enumerate(tqdm(log2_range)):
+            print('OnTAD tuning - log2: {}'.format(log2))
+            with contextlib.redirect_stdout(io.StringIO()) as f:
+                gt_rates_25kb, pred_rates_25kb = np.zeros(len(set_25kb)), np.zeros(len(set_25kb))
+                for j, f_25kb in enumerate(set_25kb):
+                    hic_mat, arrowhead_tads = load_hic_groundtruth(f_25kb, 25000)
+                    ontad = OnTAD()
+                    ontad_tads = ontad.getTADs(hic_mat, log2=log2)
+                    _, _, gt_rate_ontad, pred_rate_ontad = compare_to_groundtruth(ground_truth=arrowhead_tads, predicted_tads=ontad_tads, gap=200000)
+                    gt_rates_25kb[i,j] = gt_rate_ontad
+                    pred_rates_25kb[i,j] = pred_rate_ontad
+                results_df.loc[((results_df.resolution==25000) & (results_df.log2==log2)), 'gt_rates'] = gt_rates_25kb.mean(axis=0)
+                results_df.loc[((results_df.resolution==25000) & (results_df.log2==log2)), 'pred_rates'] = pred_rates_25kb.mean(axis=0)
+
+                gt_rates_100kb, pred_rates_100kb = np.zeros(len(set_100kb)), np.zeros(len(set_100kb))
+                for j, f_100kb in enumerate(set_100kb):
+                    hic_mat, arrowhead_tads = load_hic_groundtruth(f_100kb, 100000)
+                    ontad = OnTAD()
+                    ontad_tads = ontad.getTADs(hic_mat, log2=log2)
+                    _, _, gt_rate_ontad, pred_rate_ontad = compare_to_groundtruth(ground_truth=arrowhead_tads, predicted_tads=ontad_tads, gap=200000)
+                    gt_rates_100kb[i,j] = gt_rate_ontad
+                    pred_rates_100kb[i,j] = pred_rate_ontad
+                results_df.loc[((results_df.resolution==100000) & (results_df.log2==log2)), 'gt_rates'] = gt_rates_100kb.mean(axis=0)
+                results_df.loc[((results_df.resolution==100000) & (results_df.log2==log2)), 'pred_rates'] = pred_rates_100kb.mean(axis=0)
+        print('\OnTAD tuning - plotting')
+        width=0.4
+        ax1.bar(results_df.loc[results_df.resolution==25000]['log2']-0.2, results_df.loc[results_df.resolution==25000]['pred_rates'], width, label='Rate of Predicted TADs by OnTAD present in Ground Truth')
+        ax1.bar(results_df.loc[results_df.resolution==25000]['log2']+0.2, results_df.loc[results_df.resolution==25000]['gt_rates'], width, label='Rate of Ground Truth TADs correctly predicted by OnTAD')
+        ax1.set_xlabel('log2')
+        ax1.set_ylabel('Rate')
+        ax1.set_xticks(results_df.loc[results_df.resolution==25000]['log2'])
+        ax1.set_title('OnTAD on 25kb intrachromosomal HiC data')
+        ax1.legend()
+        ax2.bar(results_df.loc[results_df.resolution==100000]['log2']-0.2, results_df.loc[results_df.resolution==100000]['pred_rates'], width, label='Rate of Predicted TADs by OnTAD present in Ground Truth')
+        ax2.bar(results_df.loc[results_df.resolution==100000]['log2']+0.2, results_df.loc[results_df.resolution==100000]['gt_rates'], width, label='Rate of Ground Truth TADs correctly predicted by OnTAD')
+        ax2.set_xlabel('log2')
+        ax2.set_ylabel('Rate')
+        ax2.set_xticks(results_df.loc[results_df.resolution==100000]['log2'])
+        ax2.set_title('OnTAD on 100kb intrachromosomal HiC data')
+        ax2.legend()
+        plt.savefig('figures/tune_ontad_log2True-False.png')
+

bfab9fa023a4cfb8dce0ec3f64c2c34aa0da5f5e
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-22 20:39:46 +0100

    Update tuning.sh

--- tuning.sh
+++ tuning.sh
@@ -7,22 +7,22 @@ from src.data import HiCDataset
 import src.utils
 import src.metrics
 import src.tad_algo
-from src.tuning import tune_topdom, tune_tadtree
+from src.tuning import tune_topdom, tune_tadtree, tune_ontad, tune_tadbit
 
 print('Init dataset')
 dataset = HiCDataset(data_folder='data')
 dataset.build_data_dict()
 development_set, test_set = dataset.split(dev_ratio = 0.7, test_ratio=0.3)
 
-print('\nTuning TopDom...')
-print('\tParameter window')
-tune_topdom(development_set)
+# print('\nTuning TopDom...')
+# print('\tParameter window')
+# tune_topdom(development_set)
 
-print('\nTuning TADTree...')
-print('\tParameter N')
-tune_tadtree(development_set, param_ranges={'N': (100,600)})
-print('\tParameter p')
-tune_tadtree(development_set, param_ranges={'p': (2,5)})
+# print('\nTuning TADTree...')
+# print('\tParameter N')
+# tune_tadtree(development_set, param_ranges={'N': (100,600)})
+# print('\tParameter p')
+# tune_tadtree(development_set, param_ranges={'p': (2,5)})
 
 print('\nTuning OnTAD...')
 print('\tParameter penalty')

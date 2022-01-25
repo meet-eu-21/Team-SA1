5e5e7542068ec0b18c7c5c2f21bb5394afe85cb2
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-25 21:27:06 +0100

    Integrate consensus functions on the Consensus class

--- src/consensus.py
+++ src/consensus.py
@@ -58,6 +58,9 @@ def compare_TADs(obs, trues, gap):
                 in_trues=False
                 break
     return counter/len(obs)    def get_consensus(self, TADs):
+
+class ConsensusMethod(ABC):
+    def get_consensus(self, TADs):
         pass
 
 class ScoreConsensus(ConsensusMethod):
@@ -82,3 +85,44 @@ class ScoreConsensus(ConsensusMethod):
 
         self.algo_usage = {25000: [TopDom, OnTAD], 100000: [TopDom, TADtree, TADbit]}
 
+    def get_all_boundaries(self, all_algo_TADs, resolution, ctcf_width_region=4):
+        dict_pos_score = {}
+        for algo, tads in all_algo_TADs.items():
+            for tad in tads:
+                for i in range(-ctcf_width_region, ctcf_width_region+1):
+                    idx_tad = int( (tad[0]+i) / resolution)
+                    if idx_tad in dict_pos_score:
+                        dict_pos_score[idx_tad] += self.algo_scores[algo] * (1/pow(2, abs(i))) # TODO: Find which law to use (Normal? Log?)
+                    else:
+                        dict_pos_score[idx_tad] = self.algo_scores[algo] * (1/pow(2, abs(i)))
+        return dict(sorted(dict_pos_score.items(), key=lambda x:x[0]))
+
+    def construct_tads(self, dict_pos_score, resolution, lim, threshold): # TODO: Tune threshold
+        lim = int(lim/resolution)
+
+        dict_pos_score = {pos:score for pos,score in dict_pos_score.items() if score >= threshold}
+
+        positions = list(dict_pos_score.keys())
+        output_tads = {}
+        for i in range(len(positions)-1):
+            if positions[i+1]-positions[i] > lim:
+                continue
+            output_tads[(positions[i], positions[i+1])] = dict_pos_score[positions[i]] + dict_pos_score[positions[i+1]]
+        return output_tads
+    
+    def get_consensus(self, hic_mat, threshold, ctcf_width_region=4, lim=3000000):
+        all_tads = {}
+        for algo in self.algo_scores.keys():
+            if self.algo_scores[algo] == np.NaN:
+                raise ValueError('ScoreConsensus not trained')
+            elif algo in self.algo_usage[hic_mat.resolution]:
+                tad_caller = algo()
+                all_tads[algo] = tad_caller.getTADs(hic_mat)
+        return self.build_consensus(all_tads, threshold, ctcf_width_region, lim)
+
+    def build_consensus(self, all_tads, resolution, threshold, ctcf_width_region, lim):
+        extended_lists = []
+        for method,list_i in all_tads.items():
+            all_tads[method] = sorted(set(list_i))
+        scores_dic = self.get_boundaries(all_tads, resolution, ctcf_width_region)
+        return construct_tads(scores_dic, lim, threshold)

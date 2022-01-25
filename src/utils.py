2138f6eed1f2363d5f85ef91c9cef7810354ad69
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-25 08:54:12 +0100

    Add chrom_name_to_variables

--- src/utils.py
+++ src/utils.py
@@ -23,3 +23,16 @@ def SCN(D, max_iter = 10):
 		D = np.divide(D, np.maximum(1, D.sum(axis = 0)))       
 		D = np.divide(D, np.maximum(1, D.sum(axis = 1)))    
 	return (D + D.T)/2 # To make matrix symetric again
+
+def chrom_name_to_variables(chrom_name):
+	chrom = chrom_name[:4]
+	cell_type = None
+	for ct in ['GM12878', 'HMEC', 'HUVEC', 'IMR90', 'NHEK']:
+		if ct in chrom_name:
+			if not cell_type:
+				cell_type = ct
+			else:
+				raise ValueError('Multiple cell types in the same file')
+	if not cell_type:
+		raise ValueError('No cell type found in the file')
+	return chrom, cell_type

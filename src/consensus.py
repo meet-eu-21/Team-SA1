340772cbd91d2839b802d7acffca132bb1bc33fd
Simon Crouzet <simon.crouzet@gmail.com>
2022-01-27 03:18:24 +0100

    Finish implementation of BordersConsensus

--- src/consensus.py
+++ src/consensus.py
@@ -1,12 +1,12 @@
 from abc import ABC
-import logging, os, tqdm, io, contextlib
+import logging, os, tqdm, io, contextlib, json, logging
 import numpy as np
 
-
+from src.metrics import compare_to_groundtruth
 from src.data import load_hic_groundtruth
 from src.utils import chrom_name_to_variables
 from src.ctcf import bedPicks, checkCTCFcorrespondance
-from src.tad_algo import TopDom, TADtree, OnTAD, TADbit
+from src.tad_algo import TopDom, TADtree, OnTAD, TADbit, TAD_class_to_str, str_to_TAD_class
 
 reference_scores = {
         'tadtree':83.23,
@@ -72,7 +72,7 @@ class ConsensusMethod(ABC):
         pass
 
 class BordersConsensus(ConsensusMethod):
-    def __init__(self) -> None:
+    def __init__(self, ctcf_coeff=1, metrics_coeff=1) -> None:
         self.ctcf = {
                 'GM12878':'data/CTCF/GM12878/ENCFF796WRU.bed',
                 'HMEC':'data/CTCF/HMEC/ENCFF059YXD.bed',
@@ -84,14 +84,22 @@ class BordersConsensus(ConsensusMethod):
 
         # Implement score as CTCF*(M_1 + M2)
         self.algo_scores = {
-                TADtree: {25000: np.NaN, 100000: np.NaN},
-                TopDom: {25000: np.NaN, 100000: np.NaN},
+                'TADtree': {'25000': np.NaN, '100000': np.NaN},
+                'TopDom': {'25000': np.NaN, '100000': np.NaN},
                 'arrowhead': np.NaN, # Check Ground Truth
-                OnTAD: {25000: np.NaN, 100000: np.NaN}, # TODO: Check OnTAD issue on 100kb
-                TADbit: {25000: np.NaN, 100000: np.NaN} # TODO: Check TADbit performance
+                'OnTAD': {'25000': np.NaN, '100000': np.NaN}, # TODO: Check OnTAD issue on 100kb
+                'TADbit': {'25000': np.NaN, '100000': np.NaN} # TODO: Check TADbit performance
         }
 
-        self.algo_usage = {25000: [TopDom, OnTAD], 100000: [TopDom, TADtree, TADbit]}
+        self.algo_usage = {'25000': ['TopDom', 'OnTAD'], '100000': ['TopDom', 'TADtree', 'TADbit']}
+
+        self.ctcf_coeff = ctcf_coeff
+        self.metrics_coeff = metrics_coeff
+
+        score_save_path = os.path.join('saves', 'algo_scores_consensus.json')
+        if os.path.isfile(score_save_path):
+            logging.info('BordersConsensus: Loading algo scores from backup file...')
+            self.algo_scores = json.load(open(score_save_path, "r"))
 
     def get_all_boundaries(self, all_algo_TADs, resolution, ctcf_width_region=4):
         dict_pos_score = {}
@@ -100,44 +108,93 @@ class BordersConsensus(ConsensusMethod):
                 for i in range(-ctcf_width_region, ctcf_width_region+1):
                     idx_tad = int( (tad[0]+i) / resolution)
                     if idx_tad in dict_pos_score:
-                        dict_pos_score[idx_tad] += self.algo_scores[algo] * (1/pow(2, abs(i))) # TODO: Find which law to use (Normal? Log?)
+                        dict_pos_score[idx_tad] += self.algo_scores[algo]['25000'] * (1/pow(2, abs(i))) # TODO: Find which law to use (Normal? Log?)
                     else:
-                        dict_pos_score[idx_tad] = self.algo_scores[algo] * (1/pow(2, abs(i)))
+                        dict_pos_score[idx_tad] = self.algo_scores[algo]['25000'] * (1/pow(2, abs(i)))
         return dict(sorted(dict_pos_score.items(), key=lambda x:x[0]))
 
     def construct_tads(self, dict_pos_score, resolution, lim, threshold): # TODO: Tune threshold
         lim = int(lim/resolution)
 
-        dict_pos_score = {pos:score for pos,score in dict_pos_score.items() if score >= threshold}
+        dict_pos_score = {pos:score for pos,score in dict_pos_score.items() if score*100 >= threshold}
 
         positions = list(dict_pos_score.keys())
         output_tads = {}
         for i in range(len(positions)-1):
             if positions[i+1]-positions[i] > lim:
                 continue
-            output_tads[(positions[i], positions[i+1])] = dict_pos_score[positions[i]] + dict_pos_score[positions[i+1]]
+            output_tads[(int(positions[i]*resolution), int(positions[i+1]*resolution))] = dict_pos_score[positions[i]] + dict_pos_score[positions[i+1]]
         return output_tads
+
+    def get_consensus_tads(self, hic_mat, threshold=10, ctcf_width_region=4, min_tad_size=100000, max_tad_size=3000000):
+        #TODO: Implement min_tad_size
+        return [k for k in self.get_consensus(hic_mat, threshold, ctcf_width_region, max_tad_size).keys()]
     
     def get_consensus(self, hic_mat, threshold, ctcf_width_region=4, lim=3000000):
         all_tads = {}
         for algo in self.algo_scores.keys():
             if self.algo_scores[algo] == np.NaN:
                 raise ValueError('ScoreConsensus not trained')
-            elif algo in self.algo_usage[hic_mat.resolution]:
-                tad_caller = algo()
+            elif algo in self.algo_usage['{}'.format(hic_mat.resolution)]:
+                tad_caller = str_to_TAD_class(algo)()
                 all_tads[algo] = tad_caller.getTADs(hic_mat)
-        return self.build_consensus(all_tads, threshold, ctcf_width_region, lim)
+        return self.build_consensus(all_tads, hic_mat.resolution, threshold, ctcf_width_region, lim)
 
     def build_consensus(self, all_tads, resolution, threshold, ctcf_width_region, lim):
         extended_lists = []
         for method,list_i in all_tads.items():
             all_tads[method] = sorted(set(list_i))
-        scores_dic = self.get_boundaries(all_tads, resolution, ctcf_width_region)
-        return construct_tads(scores_dic, lim, threshold)
+        scores_dic = self.get_all_boundaries(all_tads, resolution, ctcf_width_region)
+        return self.construct_tads(scores_dic, resolution, lim, threshold)
 
-    def evaluateAlgorithmScore(self, development_set):
-        # TODO: Save scores in a file and reload them next time
-        logging.info('Tuning TopDom on intrachromosomal HiC data')
+    def evaluate_algorithm_score(self, development_set):
+        logging.info('Evaluating algorithm scores')
+        score_save_path = os.path.join('saves', 'algo_scores_consensus.json')
+        ctcf_scores_path = os.path.join('saves', 'ctcf_scores.json')
+        metrics_scores_path = os.path.join('saves', 'metrics_scores.json')
+        if os.path.isfile(score_save_path):
+            self.algo_scores = json.load(open(score_save_path, "r"))
+        else:
+            if os.path.isfile(ctcf_scores_path):
+                ctcf_scores = json.load(open(ctcf_scores_path, "r"))
+            else:
+                ctcf_scores = self.compute_ctcf_scores(development_set)
+                with open(ctcf_scores_path, "w") as f:
+                    json.dumps(ctcf_scores, f)
+
+            if os.path.isfile(metrics_scores_path):
+                metrics_scores = json.load(open(metrics_scores_path, "r"))
+            else:
+                metrics_scores = self.compute_metrics_scores(development_set)
+                with open(metrics_scores_path, "w") as f:
+                    json.dump(metrics_scores, f)
+            
+            for algo in self.algo_scores.keys():
+                if algo in self.algo_usage['25000']:
+                    self.algo_scores[algo]['25000'] = (self.ctcf_coeff * ctcf_scores[algo]['25000']) * (self.metrics_coeff * sum(metrics_scores[algo]['25000']))
+                if algo in self.algo_usage['100000']:
+                    self.algo_scores[algo]['100000'] = (self.ctcf_coeff * ctcf_scores[algo]['100000']) * (self.metrics_coeff * sum(metrics_scores[algo]['100000']))
+            with open(score_save_path, "w") as f:
+                json.dump(self.algo_scores, f)
+            logging.info('Algorithm scores computed and saved')
+
+
+    # def compute_ctcf_algo_score(self, algo, set, resolution):
+    #     if algo in self.algo_usage[resolution]:
+    #         ctcf_scores = []
+    #         for j, f in enumerate(set):
+    #             hic_mat, arrowhead_tads = load_hic_groundtruth(f, resolution)
+    #             tad_caller = algo()
+    #             tads = tad_caller.getTADs(hic_mat)
+    #             chrom, cell_type = chrom_name_to_variables(f)
+    #             ctcf_peaks = bedPicks(self.ctcf[cell_type], chrom)
+    #             ctcf_scores.append(checkCTCFcorrespondance(ctcf_peaks, tads))
+    #         return np.mean(ctcf_scores)
+    #     else:
+    #         return np.NaN
+            
+    def compute_ctcf_scores(self, development_set):
+        logging.info('Computing CTCF scores on intrachromosomal HiC data')
         set_25kb = []
         set_100kb = []
         for f in development_set:
@@ -148,30 +205,73 @@ class BordersConsensus(ConsensusMethod):
             else:
                 raise ValueError('File name {} was unexpected'.format(f))
 
-        ctcf_scores_25kb = {algo: [] for algo in self.algo_scores.keys()}
-        ctcf_scores_100kb = {algo: [] for algo in self.algo_scores.keys()}
+        algo_ctcf_scores = {algo: {'25000': np.NaN, '100000': np.NaN} for algo in self.algo_scores.keys()}
         with contextlib.redirect_stdout(io.StringIO()) as f:
             for algo in self.algo_scores.keys():
-                if algo in self.algo_usage[25000]:
+                if algo in self.algo_usage['25000']:
+                    ctcf_scores_25kb = []
                     for j, f_25kb in enumerate(set_25kb):                    
-                        hic_mat, arrowhead_tads = load_hic_groundtruth(f_25kb, 25000)
-                        tad_caller = algo()
+                        hic_mat, arrowhead_tads = load_hic_groundtruth(f_25kb, '25000')
+                        tad_caller = str_to_TAD_class(algo)()
                         tads = tad_caller.getTADs(hic_mat)
                         chrom, cell_type = chrom_name_to_variables(f_25kb)
                         ctcf_peaks = bedPicks(self.ctcf[cell_type], chrom)
-                        ctcf_scores_25kb[algo].append(checkCTCFcorrespondance(ctcf_peaks, tads))
-                if algo in self.algo_usage[100000]:
+                        ctcf_scores_25kb.append(checkCTCFcorrespondance(ctcf_peaks, tads))
+                    algo_ctcf_scores[algo]['25000'] = np.mean(ctcf_scores_25kb)
+                
+                if algo in self.algo_usage['100000']:
+                    ctcf_scores_100kb = []
                     for j, f_100kb in enumerate(set_100kb):
-                        hic_mat, arrowhead_tads = load_hic_groundtruth(f_100kb, 100000)
-                        tad_caller = algo()
+                        hic_mat, arrowhead_tads = load_hic_groundtruth(f_100kb, '100000')
+                        tad_caller = str_to_TAD_class(algo)()
                         tads = tad_caller.getTADs(hic_mat)
                         chrom, cell_type = chrom_name_to_variables(f_100kb)
                         ctcf_peaks = bedPicks(self.ctcf[cell_type], chrom)
-                        ctcf_scores_100kb[algo].append(checkCTCFcorrespondance(ctcf_peaks, tads))
-        print(ctcf_scores_25kb)
-        print(ctcf_scores_100kb)
+                        ctcf_scores_100kb.append(checkCTCFcorrespondance(ctcf_peaks, tads))
+                    algo_ctcf_scores[algo]['100000'] = np.mean(ctcf_scores_100kb)
+        
+        return algo_ctcf_scores
+
+    def compute_metrics_scores(self, development_set):
+        logging.info('Computing Metrics on intrachromosomal HiC data')
+        set_25kb = []
+        set_100kb = []
+        for f in development_set:
+            if '25kb' in f:
+                set_25kb.append(f)
+            elif '100kb' in f:
+                set_100kb.append(f)
+            else:
+                raise ValueError('File name {} was unexpected'.format(f))
+        
+        algo_performances = {algo: {'25000': (np.NaN, np.NaN), '100000': (np.NaN, np.NaN)} for algo in self.algo_scores.keys()}
         for algo in self.algo_scores.keys():
-            if algo in self.algo_usage[25000]:
-                self.algo_scores[algo][25000] = np.mean(ctcf_scores_25kb[algo])
-            if algo in self.algo_usage[100000]:
-                self.algo_scores[algo][100000] = np.mean(ctcf_scores_100kb[algo])
+            gt_rates_25kb, pred_rates_25kb = np.zeros(len(set_25kb)), np.zeros(len(set_25kb))
+            gt_rates_100kb, pred_rates_100kb = np.zeros(len(set_100kb)), np.zeros(len(set_100kb))
+            with contextlib.redirect_stdout(io.StringIO()) as f:
+                if algo in self.algo_usage['25000']:
+                    for i, f_25kb in enumerate(set_25kb):
+                        hic_mat, arrowhead_tads = load_hic_groundtruth(f_25kb, '25000')
+                        method = str_to_TAD_class(algo)()
+                        tads = method.getTADs(hic_mat)
+                        _, _, gt_rate_tadbit, pred_rate_tadbit = compare_to_groundtruth(ground_truth=arrowhead_tads, predicted_tads=tads)
+                        gt_rates_25kb[i] = gt_rate_tadbit
+                        pred_rates_25kb[i] = pred_rate_tadbit
+                    pred_rates_25kb = pred_rates_25kb.mean(axis=0)
+                    gt_rates_25kb = gt_rates_25kb.mean(axis=0)
+                    algo_performances[algo]['25000'] = (gt_rates_25kb, pred_rates_25kb)
+
+                if algo in self.algo_usage['100000']:
+                    for i, f_100kb in enumerate(set_100kb):
+                        hic_mat, arrowhead_tads = load_hic_groundtruth(f_100kb, '100000')
+                        method = str_to_TAD_class(algo)()
+                        tads = method.getTADs(hic_mat)
+                        _, _, gt_rate_tadbit, pred_rate_tadbit = compare_to_groundtruth(ground_truth=arrowhead_tads, predicted_tads=tads)
+                        gt_rates_100kb[i] = gt_rate_tadbit
+                        pred_rates_100kb[i] = pred_rate_tadbit
+                    pred_rates_100kb = pred_rates_100kb.mean(axis=0)
+                    gt_rates_100kb = gt_rates_100kb.mean(axis=0)
+                    algo_performances[algo]['100000'] = (gt_rates_100kb, pred_rates_100kb)
+            
+        return algo_performances
+

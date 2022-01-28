4a64c4134b14f7b0027e7d492e0093e0c976d61e
LiamLeGoffic <liamlegoffic@yahoo.fr>
2022-01-28 20:17:32 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,29 +2,62 @@ import pandas as pd
 import numpy as np
 import os
 
-# load the available ArrowHead results
+
 def read_arrowhead_result(path, chromosome):
+	"""
+    Load the available ArrowHead results and put them in a tuples list
+    ----------
+    INPUT
+    path : str
+        path to the arrowhead results
+    chromosome : str
+        name of the chromosome to looking for
+    -----------
+    OUTPUT
+    list_tads : list of tuples
+		list of the TADs predicted by Arrowhead
+    """
     # TODO: Refactor
     df = pd.read_csv(path, sep='\t', header=0)
     df = df[df['chr1']==chromosome]
     list_tads = []
     for i in df.index.tolist():
-        list_tads.append((int(df['x1'][i]), int(df['x2'][i])))
+        list_tads.append((int(df['x1'][i]), int(df['x2'][i]))) # we get the TADs coordinates from the columns named "x1" and "x2"
     return list_tads
 
 def SCN(D, max_iter = 10):
-	"""
-	Input : spare array * int
-	Out  : SCN(D)
-	Code version from hictools.py, which was inspired by Boost-HiC paper
-	"""    
-	# Iteration over max_iter    
+    """
+    Improve the matrix display in the humain readable format
+    Code version from hictools.py, which was inspired by Boost-HiC paper
+    ----------
+    INPUT
+    D : npy matrix
+        path to the arrowhead results
+    max_iter : int
+        maximum number of iterations
+    -----------
+    OUTPUT
+    return a numpy matrix
+    """
 	for i in range(max_iter):
 		D = np.divide(D, np.maximum(1, D.sum(axis = 0)))       
 		D = np.divide(D, np.maximum(1, D.sum(axis = 1)))    
 	return (D + D.T)/2 # To make matrix symetric again
 
 def chrom_name_to_variables(chrom_name):
+    """
+    Put the name the cell type of the chromosome in two variables
+    ----------
+    INPUT
+    chrom_name : str
+        path to the chromosome
+    -----------
+    OUTPUT
+    chrom : str
+		name of the chromosome
+    cell_type : str
+        name of the cell type
+    """
 	len_path = len(chrom_name.split(os.path.sep))
 	chrom = chrom_name.split(os.path.sep)[len_path-1][:4]
 	if chrom[:3] != 'chr':
@@ -34,7 +67,7 @@ def chrom_name_to_variables(chrom_name):
 		if ct in chrom_name:
 			if not cell_type:
 				cell_type = ct
-			else:
+			else: # check if only one cell type appears in the file name
 				raise ValueError('Multiple cell types in the same file')
 	if not cell_type:
 		raise ValueError('No cell type found in the file')
